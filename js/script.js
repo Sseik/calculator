748bbd4ad8a40e808959013fdee8d6a2f181482b
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2025-12-24 22:16:56 +0200

    Add function that calls math operation function based on operator

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+function operate(operator, firstOperand, secondOperand) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return subtract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
+}
