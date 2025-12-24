7ecda2abb60ef8cd11df9ae1a46420cd286bbd64
Dmytro Hapchyn <48380610+Sseik@users.noreply.github.com>
2025-12-24 23:37:50 +0200

    Add button clicks handling, implement calculator work

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let firstOperand;
-let secondOperand;
+let firstOperand = "";
+let secondOperand = "";
 let operator;
 
 function operate(operator, firstOperand, secondOperand) {
@@ -19,3 +19,87 @@ function operate(operator, firstOperand, secondOperand) {
       return divide(firstOperand, secondOperand);
   }
 }
+
+const display = document.querySelector("#display");
+
+function pressNumber(number) {
+  if (operator) {
+    secondOperand += number;
+  } else {
+    firstOperand += number;
+  }
+  display.textContent = `${firstOperand} ${operator || ""} ${secondOperand}`;
+}
+
+function pressOperator(pressedOperator) {
+  if (!secondOperand.length && !isNaN(+firstOperand)) {
+    operator = pressedOperator;
+    display.textContent = `${firstOperand} ${operator || ""}`;
+  }
+}
+
+function pressBack() {
+  if (secondOperand.length) {
+    secondOperand = secondOperand.slice(0, -1);
+  } else if (operator) {
+    operator = null;
+  } else {
+    firstOperand = firstOperand.slice(0, -1);
+  }
+  display.textContent = `${firstOperand} ${operator || ""}`;
+}
+
+function pressEquals() {
+  if (!isNaN(+firstOperand) && !isNaN(+secondOperand) && operator) {
+    display.textContent = operate(operator, +firstOperand, +secondOperand);
+    firstOperand = "";
+    secondOperand = "";
+    operator = null;
+  }
+}
+
+function pressDot() {
+  if (operator) {
+    if (secondOperand.indexOf(".") === -1) {
+      secondOperand += ".";
+    }
+  } else {
+    if (firstOperand.indexOf(".") === -1) {
+      firstOperand += ".";
+    }
+  }
+  display.textContent = `${firstOperand} ${operator || ""} ${secondOperand}`;
+}
+
+function pressClear() {
+  firstOperand = "";
+  secondOperand = "";
+  operator = null;
+  display.textContent = "";
+}
+
+document.querySelector("#buttonsContainer").addEventListener("click", (e) => {
+  if (e.target.id === "buttonsContainer") return;
+  switch (e.target.textContent) {
+    case "=":
+      pressEquals();
+      break;
+    case ".":
+      pressDot();
+      break;
+    case "Clear":
+      pressClear();
+      break;
+    case "←":
+      pressBack();
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      pressOperator(e.target.textContent);
+      break;
+    default:
+      pressNumber(e.target.textContent);
+  }
+});
